61c19077013d53c8cb51b724627f3463872de39c
Amaanjan00 <amaan.jan@bba.christuniversity.in>
2025-03-20 14:37:20 +0530

    check

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './Pages/App.jsx'
 import About from './Pages/About.jsx'
-import Artisans from './Pages/Artisans.jsx'
 import Contact from './Pages/Contact.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
@@ -11,7 +10,6 @@ const router = createBrowserRouter([
   {path: '/', element: <App/>},
   {path: '/contact', element: <Contact/>},
   {path: '/about', element: <About/>},
-  {path: '/artisans', element: <Artisans/>},
 ])
 
 createRoot(document.getElementById('root')).render(
